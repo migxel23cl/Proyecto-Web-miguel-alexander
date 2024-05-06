d581fb9cabf321d9a9786eef5e69a786e25e2e93
Migxel23cl <mi.pereirap@duocuc.cl>
2024-05-06 16:38:50 -0400

    js usuario register
    dd

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 
   $(document).ready(function() {
-  // Definir una lista para almacenar los usuarios
   var listaUsuarios = [];
 
   $('#formularioRegistro').submit(function(e) {
     e.preventDefault();
 
-    // Crear un objeto usuario con los datos del formulario
+   
     var usuario = {
       nombres: $('#nombres').val(),
       apellido: $('#apellido').val(),
@@ -29,17 +28,16 @@
     $('#password').val('');
     
 
-    // Redirigir a la página de inicio de sesión
     //window.location.href = 'login.html';
   });
   
-  // Lista de usuarios almacenados
+  
   var listaUsuarios = [
     { nombres: "Miguel", apellido: "Pereira", correo: "Miguel@gmail.com", password: "12345"},
     { nombres: "pepe", apellido: "lava", correo: "diez@gmail.com", password: "123455"},
     { nombres: "jaime", apellido: "pepe", correo: "doce@gmail.com", password: "1225"},
     
-    // Agrega más usuarios según sea necesario
+
   ];
 
   function verificarCredenciales(username, password) {
@@ -53,7 +51,7 @@
     return false;
   }
 
-  // Evento de clic en el botón de inicio de sesión
+
   $('#login-button').click(function() {
     var username = $('#username').val();
     var password = $('#password').val();
